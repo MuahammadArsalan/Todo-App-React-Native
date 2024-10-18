271ecc1c0233af369c3731b1006b7f6ae4abd621
MuahammadArsalan <muhammadarsalanforever@gmail.com>
2024-10-18 06:23:55 -0700

    Add to successfully

--- app/index.tsx
+++ app/index.tsx
@@ -1,20 +1,72 @@
-import { View, Text ,StyleSheet  } from 'react-native'
+import { View, Text ,StyleSheet ,TextInput ,SafeAreaView, FlatList,TouchableOpacity } from 'react-native'
 import React, { useState } from 'react'
-import { SafeAreaView  } from 'react-native-safe-area-context'
-import { TextInput } from 'react-native-gesture-handler'
-
 const Home = () => {
 
-const [input , setinput] = useState(null)
+const [input ,setInput] = useState('')
+  const [todo , setTodo] = useState<string[]>(['hello world'])
+// ----------------- AddTodo--------------------//
+
+function addTodo(){
+  console.log(input)
+  todo.push(input)
+  setInput([...todo])
+  // console.log(todo);
+  
+  setInput('')
+}
+// ----------------- DeleteTodo--------------------//
+
+
+
+
+let deleteTodo = (index:number) => {
+  console.log('todo deleted',index);
+  
+}
+
+
+// ----------------- EditTodo--------------------//
+
+
+let editTodo = () => {
+
+}
 
   return (
     <SafeAreaView>
     <TextInput
       style={styles.input}
-      onChangeText={setinput}
-      value={input}
+      onChangeText={setInput}
+      value ={input}
+    placeholder='Enter Todo'
     />
-    
+
+<TouchableOpacity  activeOpacity={0.7} style={styles.button} onPress={addTodo}>
+        <Text>Add Todo</Text>
+      </TouchableOpacity>
+
+
+{todo.length > 0 ? 
+  <FlatList
+        data={todo}
+        renderItem={({item ,index}) => {
+          return <View style={styles.item}>
+<Text style={styles.title}>{item}</Text>
+
+<TouchableOpacity  activeOpacity={0.7} style={styles.listbutton} onPress={addTodo}>
+        <Text onPress={() => {deleteTodo(index)}}>Delete Todo</Text>
+      </TouchableOpacity>
+<TouchableOpacity  activeOpacity={0.7} style={styles.listbutton} onPress={addTodo}>
+        <Text onPress={editTodo}>Edit Todo</Text>
+      </TouchableOpacity>
+          </View>
+
+        } }
+/>
+: null
+
+}
+
   </SafeAreaView>
   )
 }
@@ -23,10 +75,44 @@ const [input , setinput] = useState(null)
 const styles = StyleSheet.create({
   input: {
     height: 40,
-    margin: 12,
+    margin: 24,
+    marginHorizontal:44,
     borderWidth: 1,
     padding: 10,
   },
+  button: {
+    alignItems: 'center',
+    marginHorizontal:110,
+    backgroundColor: '#DDDDDD',
+    padding: 10,
+  },
+  listbutton: {
+    alignItems: 'center',
+    marginHorizontal:90,
+    borderRadius:10,
+    marginTop:10,
+    backgroundColor: '#DDDDDD',
+    padding: 10,
+  },
+  ret:{
+backgroundColor:"black",
+color:"white"
+  },
+  item: {
+    backgroundColor: '#000000',
+    borderRadius:25,
+    padding: 20,
+    marginVertical: 8,
+    marginHorizontal: 16,
+  },
+  title: {
+    fontSize: 32,
+    color: 'white',
+    textAlign: 'center'
+  },
+
+
+
 });
 
 
